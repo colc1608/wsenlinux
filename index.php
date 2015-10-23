1fa2771b56e8463261dca781c5dbdc799e9fc8d6
cesarlc <cesar.1608@outlook.com>
2015-10-23 03:02:35 -0500

    primer estructura del proyecto

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>MI INDEX</title>
-</head>
-<body>
-	<?php
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina principal</title>
+    </head>
+    <body>
 
-	print(" <H2>SALARIO DE TRABAJADOR</h2>");
+        <h1 align="center">Hola desde el servidor linux</h1>
+        <?php
 
-	?>
+        print("<h2>Hola desde php5</h2>");
 
-	<form action="recibirDatos.php" method="post">
-	Trabajador<input type="text" name="tra" size="30" /> <br>  <br>
-	Años experiencia<input type="text" name="exp" size="10" /> <br>
-	tarifa<input type="text" name="tar" size="10" /> <br> 
-	horas<input type="text" name="hor" size="10" /> <br> 
-	<input type="submit" name="b1" value=" Enviar " />
-	</form>
-	
-</body>
+        ?>
+
+        <form action="recibirDatos.php" method="post">
+            Trabajador<input type="text" name="tra" size="30" /> <br>  <br>
+            Años experiencia<input type="text" name="exp" size="10" /> <br>
+            tarifa<input type="text" name="tar" size="10" /> <br> 
+            horas<input type="text" name="hor" size="10" /> <br> 
+            <input type="submit" name="b1" value=" Enviar " />
+        </form>
+
+    </body>
 </html>
--- prueba.php
+++
@@ -1,5 +0,0 @@
-<h1>Mi primera pagina desde un servidor</h1>
-
-<?php
-echo ("hola desde PHP5");
-?>
